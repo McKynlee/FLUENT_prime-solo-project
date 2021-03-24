b40c55c1b31c19dd745dcf3fa4ff6310a5ce70dc
McKynlee Westman <mckynlee.westman@gmail.com>
2021-03-24 17:03:36 -0500

    Playing with ol on landing page, need to play with numbers

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -16,7 +16,7 @@ function LandingPage() {
   }
 
   return (
-    <div className="container">
+    <div className="landing-container">
       <h1>{heading}</h1>
       <img className="landing-logo" src={img} alt="FLUENT logo" />
       <h3>Foreign Language Universal Engagement Tool</h3>
@@ -24,42 +24,47 @@ function LandingPage() {
 
       <h4>Register to either:</h4>
 
-      <div className="landing-learner">
-        <button onClick={learnerRegistration}
-        >
-          Become a Learner:
+      <div className="landing-user-description-container">
+        <div className="landing-learner">
+          <button onClick={learnerRegistration}
+          >
+            Become a Learner:
         </button>
-        <ol>
-          <li>
-            We get to know you.
+          <ol>
+            <li>
+              We get to know you.
           </li>
-          <li>
-            You complete challenges & earn points!
+            <li>
+              Complete challenges & earn points!
           </li>
-          <li>
-            Receive tailored feedback!
+            <li>
+              Receive tailored feedback!
           </li>
-        </ol>
-        <p><em>Watch the demo here</em></p>
+          </ol>
+          <p><em>Watch the demo here</em></p>
+        </div>
+
+        <div className="landing-instructor">
+          <button onClick={instructorRegistration}>
+            Become an Instructor:
+        </button>
+          <ol>
+            <li>
+              Tell learners who you are.
+          </li>
+            <li>
+              We protect your availability.
+          </li>
+            <li>
+              Share your expertise!
+          </li>
+          </ol>
+          <p><em>Watch the demo here</em></p>
+        </div>
+
       </div>
 
-      <div className="landing-instructor">
-        <button onClick={instructorRegistration}>
-          Become an Instructor:
-        </button>
-        <ol>
-          <li>
-            Tell learners who you are.
-          </li>
-          <li>
-            We protect your availability.
-          </li>
-          <li>
-            Share your expertise!
-          </li>
-        </ol>
-        <p><em>Watch the demo here</em></p>
-      </div>
+
     </div>
   );
 }
