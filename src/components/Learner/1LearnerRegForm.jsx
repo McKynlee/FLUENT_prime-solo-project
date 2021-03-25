c76eda91458d49a9aa9faa154d09bba3a9489cc8
McKynlee Westman <mckynlee.westman@gmail.com>
2021-03-24 23:11:18 -0500

    mostly done with Learner reg 1, need to fine tune

--- src/components/Learner/1LearnerRegForm.jsx
+++ src/components/Learner/1LearnerRegForm.jsx
@@ -80,145 +80,161 @@ function LearnerRegistration1() {
   }; // end goToStep2
 
   return (
-    <form className="formPanel" onSubmit={goToStep2}>
-      <h2>Register to become FLUENT:</h2>
+    <form className="main-container" onSubmit={goToStep2}>
+      <div className="registration-title">
+        <h2 className="teal-underline">Register to become FLUENT:</h2>
+        <h3><em>Step 1 of 2</em></h3>
+        <div class="meter half">
+          <span></span>
+        </div>
+      </div>
 
-      <h3><em>Step 1 of 2</em></h3>
       {errors.registrationMessage && (
         <h3 className="alert" role="alert">
           {errors.registrationMessage}
         </h3>
       )}
-      <h3>Personal Information:</h3>
-      <h4>
-        We use this information to tailor your learning experience!
+
+      <div className="second-main-container ">
+        {/* <div className="registration-container"> */}
+        <div className="sub-container-left make-flex ">
+          <div className="justify-left">
+            <h3>Personal Information:</h3>
+            <h4>
+              We use this information to tailor your learning experience!
       </h4>
-      <p><em>*Denotes a required field.</em></p>
-      <div>
-        <label htmlFor="firstName">
-          First Name:
+            <p><em>*Denotes a required field.</em></p>
+            <div>
+              <label htmlFor="firstName">
+                First Name:
           <input
-            type="text"
-            name="firstName"
-            value={firstName}
-            required
-            onChange={(event) => setFirstName(event.target.value)}
-          />
-        </label>
-      </div>
+                  type="text"
+                  name="firstName"
+                  value={firstName}
+                  required
+                  onChange={(event) => setFirstName(event.target.value)}
+                />
+              </label>
+            </div>
 
-      <div>
-        <label htmlFor="lastName">
-          Last Name:
+            <div>
+              <label htmlFor="lastName">
+                Last Name:
           <input
-            type="text"
-            name="lastName"
-            value={lastName}
-            required
-            onChange={(event) => setLastName(event.target.value)}
-          />
-        </label>
-      </div>
+                  type="text"
+                  name="lastName"
+                  value={lastName}
+                  required
+                  onChange={(event) => setLastName(event.target.value)}
+                />
+              </label>
+            </div>
 
-      <div>
-        <label htmlFor="pronoun">
-          Your Preferred Pronouns:
+            <div className="select-dropdown-container">
+              <label htmlFor="pronoun">
+                Your Preferred Pronouns:
           <select
-            type="text"
-            name="pronoun"
-            value={pronoun}
-            required
-            onChange={(event) => setPronoun(Number(event.target.value))}
-          >
-            <option value=''>Choose One</option>
-            {pronounList.map((pronoun) => {
-              return (
-                <option key={pronoun.id}
-                  value={pronoun.id}
+                  type="text"
+                  name="pronoun"
+                  value={pronoun}
+                  required
+                  onChange={(event) => setPronoun(Number(event.target.value))}
                 >
-                  {pronoun.pronoun}
-                </option>
-              )
-            })}
-          </select>
-        </label>
-      </div>
+                  <option value=''>Choose One</option>
+                  {pronounList.map((pronoun) => {
+                    return (
+                      <option key={pronoun.id}
+                        value={pronoun.id}
+                      >
+                        {pronoun.pronoun}
+                      </option>
+                    )
+                  })}
+                </select>
+              </label>
+            </div>
 
-      <div>
-        <label htmlFor="language">
-          Language You Want to Learn:
+            <div>
+              <label htmlFor="language">
+                Language You Want to Learn:
           <select
-            type="text"
-            name="language"
-            value={targetLanguage}
-            required
-            onChange={(event) => setTargetLanguage(Number(event.target.value))}
-          >
-            <option value=''>Choose One</option>
-            {languageList.map((language) => {
-              return (
-                <option key={language.id}
-                  value={language.id}
+                  type="text"
+                  name="language"
+                  value={targetLanguage}
+                  required
+                  onChange={(event) => setTargetLanguage(Number(event.target.value))}
                 >
-                  {language.name}
-                </option>
-              )
-            })}
-          </select>
-        </label>
-      </div>
+                  <option value=''>Choose One</option>
+                  {languageList.map((language) => {
+                    return (
+                      <option key={language.id}
+                        value={language.id}
+                      >
+                        {language.name}
+                      </option>
+                    )
+                  })}
+                </select>
+              </label>
+            </div>
 
-      <div>
-        <label htmlFor="language">
-          Current Skill Level with your Selected Language:
+            <div>
+              <label htmlFor="language">
+                Current Skill Level with your Selected Language:
           <select
-            type="text"
-            name="skill"
-            value={languageSkill}
-            required
-            onChange={(event) => setLanguageSkill(Number(event.target.value))}
-          >
-            <option value=''>Choose One</option>
-            <option value='1'>1: I know a few words.</option>
-            <option value='2'>2: I can kind of make a sentence.</option>
-            <option value='3'>3: I'm feeling strong with writing sentences.</option>
-            <option value='4'>4: I can speak some.</option>
-            <option value='5'>5: I can speak well, maybe just some grammar tweaks.</option>
-          </select>
-        </label>
-      </div>
+                  type="text"
+                  name="skill"
+                  value={languageSkill}
+                  required
+                  onChange={(event) => setLanguageSkill(Number(event.target.value))}
+                >
+                  <option value=''>Choose One</option>
+                  <option value='1'>1: I know a few words.</option>
+                  <option value='2'>2: I can kind of make a sentence.</option>
+                  <option value='3'>3: I'm feeling strong with writing sentences.</option>
+                  <option value='4'>4: I can speak some.</option>
+                  <option value='5'>5: I can speak well, maybe just some grammar tweaks.</option>
+                </select>
+              </label>
+            </div>
+          </div>
+        </div>
 
-      <h3>Your Log-In Information:</h3>
-      <h4>
-        your email address and password will be
-        your log-in credentials each time you return to F.L.U.E.N.T.
+        {/* <div className="registration-container"> */}
+        <div className="sub-container-left">
+          <h3>Your Log-In Information:</h3>
+          <h4>
+            your email address and password will be
+            your log-in credentials each time you return to F.L.U.E.N.T.
         </h4>
-      <div>
-        <label htmlFor="username">
-          Email address:
+          <div>
+            <label htmlFor="username">
+              Email address:
           <input
-            type="text"
-            name="username"
-            value={username}
-            required
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password:
+                type="text"
+                name="username"
+                value={username}
+                required
+                onChange={(event) => setUsername(event.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="password">
+              Password:
           <input
-            type="password"
-            name="password"
-            value={password}
-            required
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <input className="btn" type="submit" name="submit" value="Go to Step 2" />
+                type="password"
+                name="password"
+                value={password}
+                required
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <input className="btn" type="submit" name="submit" value="Go to Step 2" />
+          </div>
+        </div>
       </div>
     </form>
   );
